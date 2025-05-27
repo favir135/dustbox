1394e86f952aecb44f391b0ab7bde70648975d8c
？ <urgwa@yahoo.co.jp>
2025-05-27 20:37:37 +0900

    エラーハンドリングを改善し、例外をログに記録するように修正

--- access_counter.js
+++ access_counter.js
@@ -6,8 +6,9 @@ async function counter() {
     let data = await response.json();
 
     document.getElementById("counter").innerText = data.count;
-  } catch {
+  } catch(e) {
     document.getElementById("counter").innerText = "エラー";
+    console.log(e);
   }
 }
 
